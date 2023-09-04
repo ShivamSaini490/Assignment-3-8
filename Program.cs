ef77916efd58a6068d7f8945b70567296a11ed82
shivam <shivam@DESKTOP-BT1O840>
2023-09-04 11:06:21 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class MyClass
+{
+    private void PrivateMethod()
+    {
+        Console.WriteLine("This is a private method.");
+    }
+
+    public void CallPrivateMethod()
+    {
+        Console.WriteLine("Calling the private method...");
+        PrivateMethod(); 
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MyClass myObject = new MyClass();
+
+        myObject.CallPrivateMethod();
+    }
+}
